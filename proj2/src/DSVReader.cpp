bae9154495fa892fc3e0369025f8eb7b4b52f6bc
Achintya Jaimini <ajaimini@ucdavis.edu>
2025-02-15 20:27:03 -0800

    Changed xml files

--- proj2/src/DSVReader.cpp
+++ proj2/src/DSVReader.cpp
@@ -110,6 +110,111 @@
 //     return DImplementation->ReadRow(row);
 // }
 
+// #include "DSVReader.h"
+// #include "StringUtils.h"
+// #include <memory>
+// #include <vector>
+// #include <string>
+
+// // Define the SImplementation struct
+// struct CDSVReader::SImplementation {
+//     std::shared_ptr<CDataSource> DataSource;
+//     std::vector<std::string> Leftover;
+//     char Delimiter;
+
+//     SImplementation(std::shared_ptr<CDataSource> src, char delimiter)
+//         : DataSource(src), Delimiter(delimiter) {}
+
+//     bool End() const {
+//         return DataSource->End() && Leftover.empty();
+//     }
+
+//     bool CheckSpecialCase(std::vector<std::string>& row) {
+//         for (size_t i = 0; i < row.size(); i++) {
+//             // If the first character of the row element is a double quote & the last character is a double quote
+//             if (row[i][0] == '\"' && row[i][row[i].size() - 1] == '\"') {
+//                 // Remove the first and last characters of the row element
+//                 row[i] = StringUtils::Slice(row[i], 1, -1);
+//                 row[i] = StringUtils::Replace(row[i], "\"\"", "\"");
+//             }
+//             // If the first character is a double quote but the last is not, or vice versa
+//             else if ((row[i][0] == '\"' && row[i][row[i].size() - 1] != '\"') ||
+//                      (row[i][0] != '\"' && row[i][row[i].size() - 1] == '\"')) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     bool ReadRow(std::vector<std::string>& row) {
+//         row.clear();
+
+//         // If the leftover vector has multiple elements, return the first row element
+//         if (Leftover.size() > 1) {
+//             row = StringUtils::Split(Leftover[0], std::string(1, Delimiter));
+//             Leftover.erase(Leftover.begin());
+
+//             if (!CheckSpecialCase(row)) {
+//                 return false;
+//             }
+//             return true;
+//         }
+
+//         // If the data source is at the end, return false
+//         if (End()) {
+//             return false;
+//         }
+
+//         // Read from the data source
+//         std::vector<char> Buffer;
+//         std::size_t BufferSize = 256;
+//         if (DataSource->Read(Buffer, BufferSize)) {
+//             std::string Str(Buffer.begin(), Buffer.end());
+
+//             // Split the string by newline after adding any previous leftover elements
+//             std::vector<std::string> Temp = StringUtils::Split(Leftover.empty() ? Str : Leftover[0] + Str, "\n");
+
+//             // Clear the leftover vector
+//             Leftover.clear();
+
+//             // Split the first element of the temp vector by the delimiter
+//             row = StringUtils::Split(Temp[0], std::string(1, Delimiter));
+
+//             // Check for special cases (e.g., mismatched quotes)
+//             if (!CheckSpecialCase(row)) {
+//                 return false;
+//             }
+
+//             // If the temp vector has more than one element, add the remaining elements to the leftover vector
+//             if (Temp.size() > 1) {
+//                 for (size_t i = 1; i < Temp.size(); i++) {
+//                     Leftover.push_back(Temp[i]);
+//                 }
+//             }
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+// // Constructor
+// CDSVReader::CDSVReader(std::shared_ptr<CDataSource> src, char delimiter) {
+//     DImplementation = std::make_unique<SImplementation>(src, delimiter);
+// }
+
+// // Destructor
+// CDSVReader::~CDSVReader() = default;
+
+// // End() function
+// bool CDSVReader::End() const {
+//     return DImplementation->End();
+// }
+
+// // ReadRow() function
+// bool CDSVReader::ReadRow(std::vector<std::string>& row) {
+//     return DImplementation->ReadRow(row);
+// }
+
 #include "DSVReader.h"
 #include "StringUtils.h"
 #include <memory>
@@ -135,12 +240,13 @@ struct CDSVReader::SImplementation {
             if (row[i][0] == '\"' && row[i][row[i].size() - 1] == '\"') {
                 // Remove the first and last characters of the row element
                 row[i] = StringUtils::Slice(row[i], 1, -1);
+                // Replace escaped quotes ("") with a single quote (")
                 row[i] = StringUtils::Replace(row[i], "\"\"", "\"");
             }
             // If the first character is a double quote but the last is not, or vice versa
             else if ((row[i][0] == '\"' && row[i][row[i].size() - 1] != '\"') ||
                      (row[i][0] != '\"' && row[i][row[i].size() - 1] == '\"')) {
-                return false;
+                return false; // Mismatched quotes
             }
         }
         return true;
@@ -155,7 +261,7 @@ struct CDSVReader::SImplementation {
             Leftover.erase(Leftover.begin());
 
             if (!CheckSpecialCase(row)) {
-                return false;
+                return false; // Mismatched quotes
             }
             return true;
         }
@@ -182,7 +288,7 @@ struct CDSVReader::SImplementation {
 
             // Check for special cases (e.g., mismatched quotes)
             if (!CheckSpecialCase(row)) {
-                return false;
+                return false; // Mismatched quotes
             }
 
             // If the temp vector has more than one element, add the remaining elements to the leftover vector
--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -1,12 +1,106 @@
+// #include "XMLWriter.h"
+// #include "DataSink.h"
+// #include <stack>
+// #include <vector>
+
+// // Private implementation struct
+// struct CXMLWriter::SImplementation {
+//     std::shared_ptr<CDataSink> DSink; // Data sink for writing XML
+//     std::stack<std::string> DEndElements; // Stack to track open elements
+
+//     // Constructor
+//     SImplementation(std::shared_ptr<CDataSink> sink)
+//         : DSink(sink) {}
+
+//     // Write a string to the data sink
+//     bool WriteString(const std::string& str) {
+//         std::vector<char> data(str.begin(), str.end());
+//         return DSink->Write(data);
+//     }
+
+//     // Write a character to the data sink
+//     bool WriteChar(char ch) {
+//         return DSink->Put(ch);
+//     }
+// };
+
+// // CXMLWriter constructor
+// CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink)
+//     : DImplementation(std::make_unique<SImplementation>(sink)) {}
+
+// // CXMLWriter destructor
+// CXMLWriter::~CXMLWriter() {
+//     Flush(); // Ensure all open elements are closed
+// }
+
+// // Flush the writer (close all open elements)
+// bool CXMLWriter::Flush() {
+//     while (!DImplementation->DEndElements.empty()) {
+//         const std::string& Element = DImplementation->DEndElements.top();
+//         DImplementation->WriteString("</");
+//         DImplementation->WriteString(Element);
+//         DImplementation->WriteChar('>');
+//         DImplementation->DEndElements.pop();
+//     }
+//     return true;
+// }
+
+// // Write an XML entity to the data sink
+// bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
+//     switch (entity.DType) {
+//         case SXMLEntity::EType::StartElement:
+//             DImplementation->WriteChar('<');
+//             DImplementation->WriteString(entity.DNameData);
+//             for (const auto& Attr : entity.DAttributes) {
+//                 DImplementation->WriteChar(' ');
+//                 DImplementation->WriteString(Attr.first);
+//                 DImplementation->WriteChar('=');
+//                 DImplementation->WriteChar('"');
+//                 DImplementation->WriteString(Attr.second);
+//                 DImplementation->WriteChar('"');
+//             }
+//             DImplementation->WriteChar('>');
+//             DImplementation->DEndElements.push(entity.DNameData);
+//             break;
+
+//         case SXMLEntity::EType::EndElement:
+//             DImplementation->WriteString("</");
+//             DImplementation->WriteString(entity.DNameData);
+//             DImplementation->WriteChar('>');
+//             break;
+
+//         case SXMLEntity::EType::CompleteElement:
+//             DImplementation->WriteChar('<');
+//             DImplementation->WriteString(entity.DNameData);
+//             for (const auto& Attr : entity.DAttributes) {
+//                 DImplementation->WriteChar(' ');
+//                 DImplementation->WriteString(Attr.first);
+//                 DImplementation->WriteChar('=');
+//                 DImplementation->WriteChar('"');
+//                 DImplementation->WriteString(Attr.second);
+//                 DImplementation->WriteChar('"');
+//             }
+//             DImplementation->WriteString(" />");
+//             break;
+
+//         case SXMLEntity::EType::CharData:
+//             DImplementation->WriteString(entity.DNameData);
+//             break;
+//     }
+//     return true;
+// }
+
 #include "XMLWriter.h"
 #include "DataSink.h"
 #include <stack>
 #include <vector>
+#include <sstream>
 
 // Private implementation struct
 struct CXMLWriter::SImplementation {
     std::shared_ptr<CDataSink> DSink; // Data sink for writing XML
     std::stack<std::string> DEndElements; // Stack to track open elements
+    int IndentLevel = 0; // Track indentation level for pretty-printing
 
     // Constructor
     SImplementation(std::shared_ptr<CDataSink> sink)
@@ -21,6 +115,32 @@ struct CXMLWriter::SImplementation {
     // Write a character to the data sink
     bool WriteChar(char ch) {
         return DSink->Put(ch);
+    }
+
+    // Escape special characters in a string
+    std::string EscapeString(const std::string& str) {
+        std::stringstream ss;
+        for (char ch : str) {
+            switch (ch) {
+                case '&': ss << "&amp;"; break;
+                case '"': ss << "&quot;"; break;
+                case '\'': ss << "&apos;"; break;
+                case '<': ss << "&lt;"; break;
+                case '>': ss << "&gt;"; break;
+                default: ss << ch; break;
+            }
+        }
+        return ss.str();
+    }
+
+    // Write indentation (tabs) based on the current indentation level
+    bool WriteIndent() {
+        for (int i = 0; i < IndentLevel; i++) {
+            if (!WriteChar('\t')) {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
@@ -37,10 +157,13 @@ CXMLWriter::~CXMLWriter() {
 bool CXMLWriter::Flush() {
     while (!DImplementation->DEndElements.empty()) {
         const std::string& Element = DImplementation->DEndElements.top();
+        DImplementation->WriteIndent();
         DImplementation->WriteString("</");
         DImplementation->WriteString(Element);
         DImplementation->WriteChar('>');
+        DImplementation->WriteChar('\n');
         DImplementation->DEndElements.pop();
+        DImplementation->IndentLevel--;
     }
     return true;
 }
@@ -49,6 +172,7 @@ bool CXMLWriter::Flush() {
 bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
     switch (entity.DType) {
         case SXMLEntity::EType::StartElement:
+            DImplementation->WriteIndent();
             DImplementation->WriteChar('<');
             DImplementation->WriteString(entity.DNameData);
             for (const auto& Attr : entity.DAttributes) {
@@ -56,20 +180,26 @@ bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
                 DImplementation->WriteString(Attr.first);
                 DImplementation->WriteChar('=');
                 DImplementation->WriteChar('"');
-                DImplementation->WriteString(Attr.second);
+                DImplementation->WriteString(DImplementation->EscapeString(Attr.second));
                 DImplementation->WriteChar('"');
             }
             DImplementation->WriteChar('>');
+            DImplementation->WriteChar('\n');
             DImplementation->DEndElements.push(entity.DNameData);
+            DImplementation->IndentLevel++;
             break;
 
         case SXMLEntity::EType::EndElement:
+            DImplementation->IndentLevel--;
+            DImplementation->WriteIndent();
             DImplementation->WriteString("</");
             DImplementation->WriteString(entity.DNameData);
             DImplementation->WriteChar('>');
+            DImplementation->WriteChar('\n');
             break;
 
         case SXMLEntity::EType::CompleteElement:
+            DImplementation->WriteIndent();
             DImplementation->WriteChar('<');
             DImplementation->WriteString(entity.DNameData);
             for (const auto& Attr : entity.DAttributes) {
@@ -77,14 +207,17 @@ bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
                 DImplementation->WriteString(Attr.first);
                 DImplementation->WriteChar('=');
                 DImplementation->WriteChar('"');
-                DImplementation->WriteString(Attr.second);
+                DImplementation->WriteString(DImplementation->EscapeString(Attr.second));
                 DImplementation->WriteChar('"');
             }
             DImplementation->WriteString(" />");
+            DImplementation->WriteChar('\n');
             break;
 
         case SXMLEntity::EType::CharData:
-            DImplementation->WriteString(entity.DNameData);
+            DImplementation->WriteIndent();
+            DImplementation->WriteString(DImplementation->EscapeString(entity.DNameData));
+            DImplementation->WriteChar('\n');
             break;
     }
     return true;
