b7fe081ffdf4e7f647c50b33feeca8c4b62931af
Achintya Jaimini <ajaimini@ucdavis.edu>
2025-02-15 20:29:36 -0800

    Changed xml files

--- proj2/src/XMLReader.cpp
+++ proj2/src/XMLReader.cpp
@@ -1,91 +1,3 @@
-// #include "XMLReader.h"
-// #include "StringUtils.h"
-// #include <memory>
-// #include <vector>
-// #include <string>
-// #include <expat.h>
-
-// struct CXMLReader::SImplementation {
-//     std::shared_ptr<CDataSource> DSource;
-//     XML_Parser DParser;
-//     SXMLEntity DCurrentEntity;
-//     bool DHasEntity;
-
-//     SImplementation(std::shared_ptr<CDataSource> src)
-//         : DSource(src), DHasEntity(false) {
-//         DParser = XML_ParserCreate(nullptr);
-//         XML_SetUserData(DParser, this);
-//         XML_SetElementHandler(DParser, StartElementHandler, EndElementHandler);
-//         XML_SetCharacterDataHandler(DParser, CharacterDataHandler);
-//     }
-
-//     ~SImplementation() {
-//         XML_ParserFree(DParser);
-//     }
-
-//     static void StartElementHandler(void *userData, const XML_Char *name, const XML_Char **atts) {
-//         SImplementation *impl = static_cast<SImplementation*>(userData);
-//         impl->DCurrentEntity.DType = SXMLEntity::EType::StartElement;
-//         impl->DCurrentEntity.DNameData = name;
-//         impl->DHasEntity = true;
-//     }
-
-//     static void EndElementHandler(void *userData, const XML_Char *name) {
-//         SImplementation *impl = static_cast<SImplementation*>(userData);
-//         impl->DCurrentEntity.DType = SXMLEntity::EType::EndElement;
-//         impl->DCurrentEntity.DNameData = name;
-//         impl->DHasEntity = true;
-//     }
-
-//     static void CharacterDataHandler(void *userData, const XML_Char *s, int len) {
-//         SImplementation *impl = static_cast<SImplementation*>(userData);
-//         impl->DCurrentEntity.DType = SXMLEntity::EType::CharData;
-//         impl->DCurrentEntity.DNameData = std::string(s, len);
-//         impl->DHasEntity = true;
-//     }
-
-//     bool ReadEntity(SXMLEntity& entity, bool skipcdata) {
-//         std::vector<char> Buffer(1024); // Use a vector for the buffer
-//         while (!DSource->End()) {
-//             size_t BytesRead = DSource->Read(Buffer, Buffer.size()); // Pass the vector to Read
-//             if (BytesRead > 0) {
-//                 if (!XML_Parse(DParser, Buffer.data(), BytesRead, XML_FALSE)) {
-//                     // Handle parsing error
-//                     XML_Error errorCode = XML_GetErrorCode(DParser);
-//                     const char *errorString = XML_ErrorString(errorCode);
-//                     // Log or handle the error
-//                     return false;
-//                 }
-//                 if (DHasEntity) {
-//                     entity = DCurrentEntity;
-//                     DHasEntity = false;
-//                     return true;
-//                 }
-//             }
-//         }
-//         XML_Parse(DParser, nullptr, 0, XML_TRUE); // Finalize parsing
-//         return false;
-//     }
-// };
-
-// // Constructor
-// CXMLReader::CXMLReader(std::shared_ptr<CDataSource> src) {
-//     DImplementation = std::make_unique<SImplementation>(src);
-// }
-
-// // Destructor
-// CXMLReader::~CXMLReader() = default;
-
-// // End() function
-// bool CXMLReader::End() const {
-//     return DImplementation->DSource->End();
-// }
-
-// // ReadEntity() function
-// bool CXMLReader::ReadEntity(SXMLEntity& entity, bool skipcdata) {
-//     return DImplementation->ReadEntity(entity, skipcdata);
-// }
-
 #include "XMLReader.h"
 #include "StringUtils.h"
 #include <memory>
@@ -115,13 +27,6 @@ struct CXMLReader::SImplementation {
         SImplementation *impl = static_cast<SImplementation*>(userData);
         impl->DCurrentEntity.DType = SXMLEntity::EType::StartElement;
         impl->DCurrentEntity.DNameData = name;
-        impl->DCurrentEntity.DAttributes.clear();
-
-        // Capture attributes
-        for (int i = 0; atts[i]; i += 2) {
-            impl->DCurrentEntity.DAttributes.emplace_back(atts[i], atts[i + 1]);
-        }
-
         impl->DHasEntity = true;
     }
 
@@ -129,7 +34,6 @@ struct CXMLReader::SImplementation {
         SImplementation *impl = static_cast<SImplementation*>(userData);
         impl->DCurrentEntity.DType = SXMLEntity::EType::EndElement;
         impl->DCurrentEntity.DNameData = name;
-        impl->DCurrentEntity.DAttributes.clear(); // Clear attributes for end elements
         impl->DHasEntity = true;
     }
 
@@ -137,7 +41,6 @@ struct CXMLReader::SImplementation {
         SImplementation *impl = static_cast<SImplementation*>(userData);
         impl->DCurrentEntity.DType = SXMLEntity::EType::CharData;
         impl->DCurrentEntity.DNameData = std::string(s, len);
-        impl->DCurrentEntity.DAttributes.clear(); // Clear attributes for character data
         impl->DHasEntity = true;
     }
 
@@ -154,11 +57,6 @@ struct CXMLReader::SImplementation {
                     return false;
                 }
                 if (DHasEntity) {
-                    if (skipcdata && DCurrentEntity.DType == SXMLEntity::EType::CharData) {
-                        // Skip CDATA if skipcdata is true
-                        DHasEntity = false;
-                        continue;
-                    }
                     entity = DCurrentEntity;
                     DHasEntity = false;
                     return true;
@@ -187,3 +85,4 @@ bool CXMLReader::End() const {
 bool CXMLReader::ReadEntity(SXMLEntity& entity, bool skipcdata) {
     return DImplementation->ReadEntity(entity, skipcdata);
 }
+
--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -1,106 +1,12 @@
-// #include "XMLWriter.h"
-// #include "DataSink.h"
-// #include <stack>
-// #include <vector>
-
-// // Private implementation struct
-// struct CXMLWriter::SImplementation {
-//     std::shared_ptr<CDataSink> DSink; // Data sink for writing XML
-//     std::stack<std::string> DEndElements; // Stack to track open elements
-
-//     // Constructor
-//     SImplementation(std::shared_ptr<CDataSink> sink)
-//         : DSink(sink) {}
-
-//     // Write a string to the data sink
-//     bool WriteString(const std::string& str) {
-//         std::vector<char> data(str.begin(), str.end());
-//         return DSink->Write(data);
-//     }
-
-//     // Write a character to the data sink
-//     bool WriteChar(char ch) {
-//         return DSink->Put(ch);
-//     }
-// };
-
-// // CXMLWriter constructor
-// CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink)
-//     : DImplementation(std::make_unique<SImplementation>(sink)) {}
-
-// // CXMLWriter destructor
-// CXMLWriter::~CXMLWriter() {
-//     Flush(); // Ensure all open elements are closed
-// }
-
-// // Flush the writer (close all open elements)
-// bool CXMLWriter::Flush() {
-//     while (!DImplementation->DEndElements.empty()) {
-//         const std::string& Element = DImplementation->DEndElements.top();
-//         DImplementation->WriteString("</");
-//         DImplementation->WriteString(Element);
-//         DImplementation->WriteChar('>');
-//         DImplementation->DEndElements.pop();
-//     }
-//     return true;
-// }
-
-// // Write an XML entity to the data sink
-// bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
-//     switch (entity.DType) {
-//         case SXMLEntity::EType::StartElement:
-//             DImplementation->WriteChar('<');
-//             DImplementation->WriteString(entity.DNameData);
-//             for (const auto& Attr : entity.DAttributes) {
-//                 DImplementation->WriteChar(' ');
-//                 DImplementation->WriteString(Attr.first);
-//                 DImplementation->WriteChar('=');
-//                 DImplementation->WriteChar('"');
-//                 DImplementation->WriteString(Attr.second);
-//                 DImplementation->WriteChar('"');
-//             }
-//             DImplementation->WriteChar('>');
-//             DImplementation->DEndElements.push(entity.DNameData);
-//             break;
-
-//         case SXMLEntity::EType::EndElement:
-//             DImplementation->WriteString("</");
-//             DImplementation->WriteString(entity.DNameData);
-//             DImplementation->WriteChar('>');
-//             break;
-
-//         case SXMLEntity::EType::CompleteElement:
-//             DImplementation->WriteChar('<');
-//             DImplementation->WriteString(entity.DNameData);
-//             for (const auto& Attr : entity.DAttributes) {
-//                 DImplementation->WriteChar(' ');
-//                 DImplementation->WriteString(Attr.first);
-//                 DImplementation->WriteChar('=');
-//                 DImplementation->WriteChar('"');
-//                 DImplementation->WriteString(Attr.second);
-//                 DImplementation->WriteChar('"');
-//             }
-//             DImplementation->WriteString(" />");
-//             break;
-
-//         case SXMLEntity::EType::CharData:
-//             DImplementation->WriteString(entity.DNameData);
-//             break;
-//     }
-//     return true;
-// }
-
 #include "XMLWriter.h"
 #include "DataSink.h"
 #include <stack>
 #include <vector>
-#include <sstream>
 
 // Private implementation struct
 struct CXMLWriter::SImplementation {
     std::shared_ptr<CDataSink> DSink; // Data sink for writing XML
     std::stack<std::string> DEndElements; // Stack to track open elements
-    int IndentLevel = 0; // Track indentation level for pretty-printing
 
     // Constructor
     SImplementation(std::shared_ptr<CDataSink> sink)
@@ -115,32 +21,6 @@ struct CXMLWriter::SImplementation {
     // Write a character to the data sink
     bool WriteChar(char ch) {
         return DSink->Put(ch);
-    }
-
-    // Escape special characters in a string
-    std::string EscapeString(const std::string& str) {
-        std::stringstream ss;
-        for (char ch : str) {
-            switch (ch) {
-                case '&': ss << "&amp;"; break;
-                case '"': ss << "&quot;"; break;
-                case '\'': ss << "&apos;"; break;
-                case '<': ss << "&lt;"; break;
-                case '>': ss << "&gt;"; break;
-                default: ss << ch; break;
-            }
-        }
-        return ss.str();
-    }
-
-    // Write indentation (tabs) based on the current indentation level
-    bool WriteIndent() {
-        for (int i = 0; i < IndentLevel; i++) {
-            if (!WriteChar('\t')) {
-                return false;
-            }
-        }
-        return true;
     }
 };
 
@@ -157,13 +37,10 @@ CXMLWriter::~CXMLWriter() {
 bool CXMLWriter::Flush() {
     while (!DImplementation->DEndElements.empty()) {
         const std::string& Element = DImplementation->DEndElements.top();
-        DImplementation->WriteIndent();
         DImplementation->WriteString("</");
         DImplementation->WriteString(Element);
         DImplementation->WriteChar('>');
-        DImplementation->WriteChar('\n');
         DImplementation->DEndElements.pop();
-        DImplementation->IndentLevel--;
     }
     return true;
 }
@@ -172,7 +49,6 @@ bool CXMLWriter::Flush() {
 bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
     switch (entity.DType) {
         case SXMLEntity::EType::StartElement:
-            DImplementation->WriteIndent();
             DImplementation->WriteChar('<');
             DImplementation->WriteString(entity.DNameData);
             for (const auto& Attr : entity.DAttributes) {
@@ -180,26 +56,20 @@ bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
                 DImplementation->WriteString(Attr.first);
                 DImplementation->WriteChar('=');
                 DImplementation->WriteChar('"');
-                DImplementation->WriteString(DImplementation->EscapeString(Attr.second));
+                DImplementation->WriteString(Attr.second);
                 DImplementation->WriteChar('"');
             }
             DImplementation->WriteChar('>');
-            DImplementation->WriteChar('\n');
             DImplementation->DEndElements.push(entity.DNameData);
-            DImplementation->IndentLevel++;
             break;
 
         case SXMLEntity::EType::EndElement:
-            DImplementation->IndentLevel--;
-            DImplementation->WriteIndent();
             DImplementation->WriteString("</");
             DImplementation->WriteString(entity.DNameData);
             DImplementation->WriteChar('>');
-            DImplementation->WriteChar('\n');
             break;
 
         case SXMLEntity::EType::CompleteElement:
-            DImplementation->WriteIndent();
             DImplementation->WriteChar('<');
             DImplementation->WriteString(entity.DNameData);
             for (const auto& Attr : entity.DAttributes) {
@@ -207,17 +77,14 @@ bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
                 DImplementation->WriteString(Attr.first);
                 DImplementation->WriteChar('=');
                 DImplementation->WriteChar('"');
-                DImplementation->WriteString(DImplementation->EscapeString(Attr.second));
+                DImplementation->WriteString(Attr.second);
                 DImplementation->WriteChar('"');
             }
             DImplementation->WriteString(" />");
-            DImplementation->WriteChar('\n');
             break;
 
         case SXMLEntity::EType::CharData:
-            DImplementation->WriteIndent();
-            DImplementation->WriteString(DImplementation->EscapeString(entity.DNameData));
-            DImplementation->WriteChar('\n');
+            DImplementation->WriteString(entity.DNameData);
             break;
     }
     return true;
