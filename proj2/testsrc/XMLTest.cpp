7115c1299d2fe812e796c7e0ec4adedc8aba7d62
Pranav Arun Tirumalai <patirumalai@ucdavis.edu>
2025-02-17 14:48:08 -0800

    Seeing if the issue is isolated to XML

--- proj2/testsrc/XMLTest.cpp
+++ proj2/testsrc/XMLTest.cpp
@@ -1,68 +1,68 @@
-#include <gtest/gtest.h>
-#include "XMLReader.h"
-#include "XMLWriter.h"
-#include "StringDataSource.h"
-#include "StringDataSink.h"
+// #include <gtest/gtest.h>
+// #include "XMLReader.h"
+// #include "XMLWriter.h"
+// #include "StringDataSource.h"
+// #include "StringDataSink.h"
 
-// Tests for CXMLWriter
-TEST(CXMLWriterTest, WriteEntityTest) {
-    auto Sink = std::make_shared<CStringDataSink>(); // Wrap in shared_ptr
-    CXMLWriter Writer(Sink);
+// // Tests for CXMLWriter
+// TEST(CXMLWriterTest, WriteEntityTest) {
+//     auto Sink = std::make_shared<CStringDataSink>(); // Wrap in shared_ptr
+//     CXMLWriter Writer(Sink);
 
-    SXMLEntity Entity;
-    Entity.DType = SXMLEntity::EType::StartElement;
-    Entity.DNameData = "root";
-    EXPECT_TRUE(Writer.WriteEntity(Entity));
-    EXPECT_EQ(Sink->String(), "<root>");
-}
+//     SXMLEntity Entity;
+//     Entity.DType = SXMLEntity::EType::StartElement;
+//     Entity.DNameData = "root";
+//     EXPECT_TRUE(Writer.WriteEntity(Entity));
+//     EXPECT_EQ(Sink->String(), "<root>");
+// }
 
-TEST(CXMLWriterTest, WriteEntityWithAttributesTest) {
-    auto Sink = std::make_shared<CStringDataSink>(); // Wrap in shared_ptr
-    CXMLWriter Writer(Sink);
+// TEST(CXMLWriterTest, WriteEntityWithAttributesTest) {
+//     auto Sink = std::make_shared<CStringDataSink>(); // Wrap in shared_ptr
+//     CXMLWriter Writer(Sink);
 
-    SXMLEntity Entity;
-    Entity.DType = SXMLEntity::EType::StartElement;
-    Entity.DNameData = "root";
-    Entity.DAttributes.push_back({"attr1", "value1"});
-    EXPECT_TRUE(Writer.WriteEntity(Entity));
-    EXPECT_EQ(Sink->String(), "<root attr1=\"value1\">");
-}
+//     SXMLEntity Entity;
+//     Entity.DType = SXMLEntity::EType::StartElement;
+//     Entity.DNameData = "root";
+//     Entity.DAttributes.push_back({"attr1", "value1"});
+//     EXPECT_TRUE(Writer.WriteEntity(Entity));
+//     EXPECT_EQ(Sink->String(), "<root attr1=\"value1\">");
+// }
 
-// Tests for CXMLReader
-TEST(CXMLReaderTest, ReadEntityTest) {
-    std::string Input = "<root>Hello, World!</root>";
-    auto Source = std::make_shared<CStringDataSource>(Input); // Wrap in shared_ptr
-    CXMLReader Reader(Source);
+// // Tests for CXMLReader
+// TEST(CXMLReaderTest, ReadEntityTest) {
+//     std::string Input = "<root>Hello, World!</root>";
+//     auto Source = std::make_shared<CStringDataSource>(Input); // Wrap in shared_ptr
+//     CXMLReader Reader(Source);
 
-    SXMLEntity Entity;
-    EXPECT_TRUE(Reader.ReadEntity(Entity));
-    EXPECT_EQ(Entity.DType, SXMLEntity::EType::StartElement);
-    EXPECT_EQ(Entity.DNameData, "root");
+//     SXMLEntity Entity;
+//     EXPECT_TRUE(Reader.ReadEntity(Entity));
+//     EXPECT_EQ(Entity.DType, SXMLEntity::EType::StartElement);
+//     EXPECT_EQ(Entity.DNameData, "root");
 
-    EXPECT_TRUE(Reader.ReadEntity(Entity));
-    EXPECT_EQ(Entity.DType, SXMLEntity::EType::CharData);
-    EXPECT_EQ(Entity.DNameData, "Hello, World!");
+//     EXPECT_TRUE(Reader.ReadEntity(Entity));
+//     EXPECT_EQ(Entity.DType, SXMLEntity::EType::CharData);
+//     EXPECT_EQ(Entity.DNameData, "Hello, World!");
 
-    EXPECT_TRUE(Reader.ReadEntity(Entity));
-    EXPECT_EQ(Entity.DType, SXMLEntity::EType::EndElement);
-    EXPECT_EQ(Entity.DNameData, "root");
-}
+//     EXPECT_TRUE(Reader.ReadEntity(Entity));
+//     EXPECT_EQ(Entity.DType, SXMLEntity::EType::EndElement);
+//     EXPECT_EQ(Entity.DNameData, "root");
+// }
 
-TEST(CXMLReaderTest, ReadEntityWithAttributesTest) {
-    std::string Input = "<root attr1=\"value1\">";
-    auto Source = std::make_shared<CStringDataSource>(Input); // Wrap in shared_ptr
-    CXMLReader Reader(Source);
+// TEST(CXMLReaderTest, ReadEntityWithAttributesTest) {
+//     std::string Input = "<root attr1=\"value1\">";
+//     auto Source = std::make_shared<CStringDataSource>(Input); // Wrap in shared_ptr
+//     CXMLReader Reader(Source);
 
-    SXMLEntity Entity;
-    EXPECT_TRUE(Reader.ReadEntity(Entity));
-    EXPECT_EQ(Entity.DType, SXMLEntity::EType::StartElement);
-    EXPECT_EQ(Entity.DNameData, "root");
-    EXPECT_EQ(Entity.DAttributes.size(), size_t(1)); // Fix signed/unsigned comparison
-    EXPECT_EQ(Entity.DAttributes[0].first, "attr1");
-    EXPECT_EQ(Entity.DAttributes[0].second, "value1");
-}
+//     SXMLEntity Entity;
+//     EXPECT_TRUE(Reader.ReadEntity(Entity));
+//     EXPECT_EQ(Entity.DType, SXMLEntity::EType::StartElement);
+//     EXPECT_EQ(Entity.DNameData, "root");
+//     EXPECT_EQ(Entity.DAttributes.size(), size_t(1)); // Fix signed/unsigned comparison
+//     EXPECT_EQ(Entity.DAttributes[0].first, "attr1");
+//     EXPECT_EQ(Entity.DAttributes[0].second, "value1");
+// }
 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+// int main(int argc, char **argv) {
+//     testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
