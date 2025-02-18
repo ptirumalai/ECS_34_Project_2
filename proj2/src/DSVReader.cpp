88041a0dc7db56e53575f58c7c6e67cd8d2d7880
Achintya Jaimini <ajaimini@ucdavis.edu>
2025-02-17 23:12:12 -0800

    Changed DSVReader

--- proj2/src/DSVReader.cpp
+++ proj2/src/DSVReader.cpp
@@ -17,84 +17,73 @@ struct CDSVReader::SImplementation {
     }
 
     void SplitRow(std::vector<std::string> &row, const std::string& content, char delimiter) {
-        // std::vector<std::string> Fields;
         std::string Field;
         bool InQuotes = false;
 
         for (size_t i = 0; i < content.size(); i++) {
             char ch = content[i];
-            // Account for being at the end of the row
             char next_ch = (i + 1) < content.size() ? content[i + 1] : '\0';
-            if (ch == '\"' && next_ch != '\"') {    
-                InQuotes = !InQuotes;
-                Field += ch;
-            } else if (InQuotes && ch == '\"' && next_ch == '\"') {
-                Field += ch;
-                i++;
-            } else if (ch == delimiter && !InQuotes) {
+
+            if (ch == '\"') {
+                if (!InQuotes) {
+                    InQuotes = true;
+                } else if (next_ch == '\"') { 
+                    Field += ch;
+                    i++;
+                } else {
+                    InQuotes = false;
+                }
+            } 
+            else if (ch == delimiter && !InQuotes) {
                 row.push_back(Field);
                 Field.clear();
-            } else {
+            } 
+            else {
                 Field += ch;
-            }                      
+            }
         }
         
-        if (!Field.empty()) {
-            row.push_back(Field);
-        }
+        row.push_back(Field); // Ensure last field is added
     }
 
     bool ReadRow (std::vector<std::string> &row){
         row.clear();
-
-        // Define an empty character vector
         std::vector<char> buf;
         bool InQuotes = false;
-        char ch;
-        char next_ch;
-        while(!End()){
-            if (DataSource->Get(ch)){
-                if (DataSource->Peek(next_ch)){    
-                    if (ch == '\"' && !InQuotes){
-                        InQuotes = !InQuotes;
-                        buf.push_back(ch);
-                    }
-                    else if (ch == '\"' && next_ch == Delimiter && InQuotes){
-                        InQuotes = !InQuotes;
-                        buf.push_back(ch);
-                    }
-                    else if (InQuotes && ch == '\"' && (next_ch == '\n' || next_ch == '\r')){
-                        buf.push_back(ch);
-                        DataSource->Get(next_ch);
-                        break;
-                    }
-                    else if (ch == '\n' && !InQuotes){
-                        break;
-                    }
-                    else if (ch == '\r' && !InQuotes){
-                        if (DataSource->Peek(next_ch)){
-                            if (next_ch == '\n'){
-                                DataSource->Get(next_ch);
-                            }
+        char ch, next_ch;
+
+        while (!End()) {
+            if (DataSource->Get(ch)) {
+                if (DataSource->Peek(next_ch)) {
+                    if (ch == '\"') {
+                        if (!InQuotes) {
+                            InQuotes = true;
+                        } else if (next_ch == '\"') {
+                            buf.push_back(ch);
+                            DataSource->Get(next_ch);
+                        } else {
+                            InQuotes = false;
+                        }
+                    } 
+                    else if (!InQuotes && (ch == '\n' || ch == '\r')) {
+                        if (ch == '\r' && next_ch == '\n') {
+                            DataSource->Get(next_ch);
                         }
                         break;
-                    }
-                    else{
+                    } 
+                    else {
                         buf.push_back(ch);
                     }
                 }
             }
-            // std::string data(buf.begin(), buf.end());
-            // std::cout << "buf: " << data << std::endl;
         }
-                    
-        // Define a string variable to store the data read from the data source
+
         std::string data(buf.begin(), buf.end());
-        // std::cout << "Data: " << data << std::endl;
         SplitRow(row, data, Delimiter);
         return !row.empty();
     }
 };
+
 CDSVReader::CDSVReader(std::shared_ptr<CDataSource> src, char delimiter) {
     DImplementation = std::make_unique<SImplementation>(src, delimiter);
 }
