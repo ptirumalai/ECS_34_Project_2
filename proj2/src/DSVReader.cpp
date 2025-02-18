f873afd0dcf435db1be00073a835f2c01ca28f00
Pranav Arun Tirumalai <patirumalai@ucdavis.edu>
2025-02-17 21:28:14 -0800

    Checking DSV Reader

--- proj2/src/DSVReader.cpp
+++ proj2/src/DSVReader.cpp
@@ -16,82 +16,146 @@ struct CDSVReader::SImplementation {
         return DataSource->End();
     }
 
+    // void SplitRow(std::vector<std::string> &row, const std::string& content, char delimiter) {
+    //     // std::vector<std::string> Fields;
+    //     std::string Field;
+    //     bool InQuotes = false;
+
+    //     for (size_t i = 0; i < content.size(); i++) {
+    //         char ch = content[i];
+    //         // Account for being at the end of the row
+    //         char next_ch = (i + 1) < content.size() ? content[i + 1] : '\0';
+    //         if (ch == '\"' && next_ch != '\"') {    
+    //             InQuotes = !InQuotes;
+    //         } else if (InQuotes && ch == '\"' && next_ch == '\"') {
+    //             Field += ch;
+    //             i++;
+    //         } else if (ch == delimiter && !InQuotes) {
+    //             row.push_back(Field);
+    //             Field.clear();
+    //         } else {
+    //             Field += ch;
+    //         }                      
+    //     }
+        
+    //     if (!Field.empty()) {
+    //         row.push_back(Field);
+    //     }
+    // }
+
+    // bool ReadRow (std::vector<std::string> &row){
+    //     row.clear();
+
+    //     // Define an empty character vector
+    //     std::vector<char> buf;
+    //     bool InQuotes = false;
+    //     char ch;
+    //     char next_ch;
+    //     while(!End()){
+    //         if (DataSource->Get(ch)){
+    //             if (DataSource->Peek(next_ch)){    
+    //                 if (ch == '\"' && !InQuotes){
+    //                     InQuotes = !InQuotes;
+    //                     buf.push_back(ch);
+    //                 }
+    //                 else if (ch == '\"' && next_ch == Delimiter && InQuotes){
+    //                     InQuotes = !InQuotes;
+    //                     buf.push_back(ch);
+    //                 }
+    //                 else if (ch == '\"' && next_ch == '\n' && InQuotes){
+    //                     buf.push_back(ch);
+    //                     DataSource->Get(next_ch);
+    //                     break;
+    //                 }
+    //                 else if (ch == '\n' && !InQuotes){
+    //                     break;
+    //                 }
+    //                 else if (ch == '\r' && !InQuotes){
+    //                     if (DataSource->Peek(next_ch)){
+    //                         if (next_ch == '\n'){
+    //                             DataSource->Get(next_ch);
+    //                         }
+    //                     }
+    //                     break;
+    //                 }
+    //                 else{
+    //                     buf.push_back(ch);
+    //                 }
+    //             }
+    //         }
+    //         // std::string data(buf.begin(), buf.end());
+    //         // std::cout << "buf: " << data << std::endl;
+    //     }
+                    
+    //     // Define a string variable to store the data read from the data source
+    //     std::string data(buf.begin(), buf.end());
+    //     // std::cout << "Data: " << data << std::endl;
+    //     SplitRow(row, data, Delimiter);
+    //     return true;
+    // }
+
     void SplitRow(std::vector<std::string> &row, const std::string& content, char delimiter) {
-        // std::vector<std::string> Fields;
         std::string Field;
         bool InQuotes = false;
 
         for (size_t i = 0; i < content.size(); i++) {
             char ch = content[i];
-            // Account for being at the end of the row
             char next_ch = (i + 1) < content.size() ? content[i + 1] : '\0';
-            if (ch == '\"' && next_ch != '\"') {    
-                InQuotes = !InQuotes;
-            } else if (InQuotes && ch == '\"' && next_ch == '\"') {
-                Field += ch;
-                i++;
+
+            if (ch == '\"') {
+                if (InQuotes && next_ch == '\"') {
+                    // Handle escaped quote
+                    Field += ch;
+                    i++;
+                } else {
+                    InQuotes = !InQuotes;
+                }
             } else if (ch == delimiter && !InQuotes) {
                 row.push_back(Field);
                 Field.clear();
             } else {
                 Field += ch;
-            }                      
+            }
         }
-        
+
         if (!Field.empty()) {
             row.push_back(Field);
         }
     }
 
-    bool ReadRow (std::vector<std::string> &row){
+    bool ReadRow(std::vector<std::string> &row) {
         row.clear();
 
-        // Define an empty character vector
         std::vector<char> buf;
         bool InQuotes = false;
         char ch;
         char next_ch;
-        while(!End()){
-            if (DataSource->Get(ch)){
-                if (DataSource->Peek(next_ch)){    
-                    if (ch == '\"' && !InQuotes){
+
+        while (!End()) {
+            if (DataSource->Get(ch)) {
+                if (DataSource->Peek(next_ch)) {
+                    if (ch == '\"') {
                         InQuotes = !InQuotes;
                         buf.push_back(ch);
-                    }
-                    else if (ch == '\"' && next_ch == Delimiter && InQuotes){
-                        InQuotes = !InQuotes;
+                    } else if (ch == Delimiter && !InQuotes) {
                         buf.push_back(ch);
-                    }
-                    else if (ch == '\"' && next_ch == '\n' && InQuotes){
-                        buf.push_back(ch);
-                        DataSource->Get(next_ch);
+                    } else if (ch == '\n' && !InQuotes) {
                         break;
-                    }
-                    else if (ch == '\n' && !InQuotes){
-                        break;
-                    }
-                    else if (ch == '\r' && !InQuotes){
-                        if (DataSource->Peek(next_ch)){
-                            if (next_ch == '\n'){
-                                DataSource->Get(next_ch);
-                            }
+                    } else if (ch == '\r' && !InQuotes) {
+                        if (DataSource->Peek(next_ch) && next_ch == '\n') {
+                            DataSource->Get(next_ch);
                         }
                         break;
-                    }
-                    else{
+                    } else {
                         buf.push_back(ch);
                     }
                 }
             }
-            // std::string data(buf.begin(), buf.end());
-            // std::cout << "buf: " << data << std::endl;
         }
-                    
-        // Define a string variable to store the data read from the data source
+
         std::string data(buf.begin(), buf.end());
-        // std::cout << "Data: " << data << std::endl;
         SplitRow(row, data, Delimiter);
-        return true;
+        return !row.empty();
     }
 };
 
